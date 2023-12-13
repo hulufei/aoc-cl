63752f3a69e584407d948e26df423ee139a29025
hulufei <ihulufei@icloud.com>
2023-12-13 14:12:32 +0800

    day12 part2

--- 2023/day12.lisp
+++ 2023/day12.lisp
@@ -5,46 +5,68 @@
 
 (defparameter *lines* (utils:read-lines 2023 12 :SAMPLE nil))
 
-(defun parse-line (line)
+(defun join-str (s n &key (separator ""))
+  (let ((control-str (format nil "~~{~~a~~^~a~~}" separator)))
+    (format
+      nil
+      control-str
+      (loop repeat n collect s)
+      separator)))
+
+(defun parse-line (line &optional (unfold 1))
   (let ((pair (uiop:split-string line :SEPARATOR " ")))
-    (list (first pair)
+    (list (join-str (first pair) unfold :SEPARATOR "?")
           (utils:parse-integer-in-line 
-            (second pair)
+            (join-str (second pair) unfold :SEPARATOR ",")
             :SEPARATOR ","))))
 
-(defun matches (seq sizes)
-  (cond ((and (null sizes)
-              (zerop (count #\# seq))) 1)
-        ((string= seq "") 0)
-        ((null sizes) 0)
-        ((char= (char seq 0) #\.) 
-         (matches (subseq seq 1) sizes))
-        ((char= (char seq 0) #\?)
-         (+ (progn
-              (setf (char seq 0) #\.)
-              (matches seq sizes))
-            (progn
-              (setf (char seq 0) #\#)
-              (matches seq sizes))))
-        ((char= (char seq 0) #\#)
-         (loop
-           with size = (first sizes)
-           with len = (length seq)
-           for i from 0 below size
-           when (or (< len size)
-                    (char= (char seq i) #\.))
-           return 0
-           finally
-           (if (and (> len size)
-                    (char= (char seq size) #\#))
-               (return 0)
-               (return
-                 (matches 
-                     (subseq
-                       seq 
-                       (if (> len size) (1+ size) size))
-                   (rest sizes))))))
-        (t 0)))
+(assert
+  (equal (parse-line ".# 1") '(".#" (1))))
+(assert
+  (equal (parse-line ".# 1" 5) '(".#?.#?.#?.#?.#" (1 1 1 1 1))))
+
+(defun matches (seq sizes &optional cache)
+  (let ((key (cons seq sizes)))
+    (or 
+      (and cache (gethash key cache))
+      (let ((result
+              (cond
+                ((and (null sizes)
+                      (zerop (count #\# seq))) 1)
+                ((string= seq "") 0)
+                ((null sizes) 0)
+                ((char= (char seq 0) #\.) 
+                 (matches (subseq seq 1) sizes cache))
+                ((char= (char seq 0) #\?)
+                 (+ (progn
+                      (setf (char seq 0) #\.)
+                      (matches seq sizes cache))
+                    (progn
+                      (setf (char seq 0) #\#)
+                      (matches seq sizes cache))))
+                ((char= (char seq 0) #\#)
+                 (loop
+                   with size = (first sizes)
+                   with len = (length seq)
+                   for i from 0 below size
+                   when (or (< len size)
+                            (char= (char seq i) #\.))
+                   return 0
+                   finally
+                   (if (and (> len size)
+                            (char= (char seq size) #\#))
+                       (return 0)
+                       (return
+                         (matches 
+                           (subseq
+                             seq 
+                             (if (> len size) (1+ size) size))
+                           (rest sizes)
+                           cache)))))
+                (t 0))))
+        (when (and cache (not (gethash key cache)))
+          (setf (gethash key cache) result))
+        result))))
 
 (assert (equal (matches "???.###" '(1 1 3)) 1))
 (assert (equal (matches ".??..??...?##." '(1 1 3)) 4))
@@ -59,3 +81,14 @@
     sum (matches seq sizes)))
 
 (part1)
+
+(defun parse-line-unfold (line)
+  (parse-line line 5))
+
+(defun part2 ()
+  (loop
+    with cache = (make-hash-table :TEST #'equal)
+    for (seq sizes) in (mapcar #'parse-line-unfold *LINES*)
+    sum (matches seq sizes cache)))
+
+(assert (equal (part2) 60681419004564))
