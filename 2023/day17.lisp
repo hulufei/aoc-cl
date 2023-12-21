b77a3a4ccb93398193f8614539d3f4ad391c1415
hulufei <ihulufei@icloud.com>
2023-12-21 15:26:42 +0800

    day17 part2: abstract tree search

--- 2023/day17.lisp
+++ 2023/day17.lisp
@@ -3,74 +3,127 @@
 
 (in-package :2023/DAY17)
 
+;;; Search implementation references:
+;;; https://www.redblobgames.com/pathfinding/a-star/introduction.html
+;;; https://github.com/norvig/paip-lisp/blob/main/docs/chapter6.md#64-a-set-of-searching-tools
+
 (defparameter *directions* '((N . (-1 . 0))
                              (S . (1 . 0))
                              (E . (0 . 1))
                              (W . (0 . -1))))
 
+(defun reverse-direction (d)
+  (ecase d
+    (N 'S)
+    (S 'N)
+    (W 'E)
+    (E 'W)))
+
 (defstruct reached-point
-  pos
+  (pos)
   (come-from nil)
   (moves '(0 0))
   (cost 0))
 
+(defun tree-search (states goal-p successors combiner)
+  "Find a states that satisfies goal-p. Start with states,
+  and search according to successors and combiner."
+  (cond ((null states) 'fail)
+        ((funcall goal-p (first states)) (first states))
+        (t (tree-search
+             (funcall combiner
+                      (funcall successors (first states))
+                      (rest states))
+             goal-p successors combiner))))
+
+(defun reached-p (grid)
+  (destructuring-bind (n m) (array-dimensions grid)
+    #'(lambda (point) 
+        (equal (reached-point-pos point)
+               (cons (1- n)
+                     (1- m))))))
+
 (defun value (grid pos)
   (aref grid (car pos) (cdr pos)))
 
-(defun move (moves d)
-  (let ((v (first moves))
-        (h (second moves)))
-    (ecase d
-      ((N S) (list (+ v 1) 0))
-      ((W E) (list 0 (+ h 1))))))
+(defun visited-key (point)
+  (list (reached-point-pos point)
+        (reached-point-moves point)
+        (reached-point-come-from point)))
 
-(defun valid-moves-p (moves)
-  (and (< (first moves) 4)
-       (< (second moves) 4)))
+(defun record-if-new-low-cost (visited p)
+  (when (< (reached-point-cost p)
+           (gethash (visited-key p) visited most-positive-fixnum))
+    (setf (gethash (visited-key p) visited)
+          (reached-point-cost p))
+    p)
+  )
 
-(defun neighbors (grid point)
-  (loop
-    with pos = (reached-point-pos point)
-    with acc-moves = (reached-point-moves point)
-    with come-from = (reached-point-come-from point)
-    with cost-so-far = (reached-point-cost point)
-    with (i . j) = pos
-    for d in '(N S W E)
-    for (x . y) = (cdr (assoc d *directions*))
-    for new-pos = (cons (+ i x) (+ j y))
-    for new-moves = (move acc-moves d)
-    when (and (not (equal come-from new-pos))
-              (utils:inside-p grid new-pos)
-              (valid-moves-p new-moves))
-    collect (make-reached-point
+(defun move (grid visited point d &optional (baby-steps 1))
+  (let* ((moves (reached-point-moves point))
+         (pos (reached-point-pos point))
+         (cost-so-far (reached-point-cost point))
+         (offset (cdr (assoc d *directions*)))
+         (v (first moves))
+         (h (second moves)))
+    (labels ((steps (mov)
+               (if (zerop mov) baby-steps 1))
+             (next-pos (start mov)
+               (cons (+ (car start) (* (car offset)
+                                       (steps mov)))
+                     (+ (cdr start) (* (cdr offset)
+                                       (steps mov)))))
+             (next-cost (start move-steps cost)
+               (cond ((zerop move-steps) cost)
+                     (t (let ((next (next-pos start 1)))
+                          (next-cost 
+                            next
+                            (1- move-steps)
+                            (+ cost (value grid next))))))))
+      (destructuring-bind (new-pos new-moves move-steps)
+          (ecase d
+            ((N S) (list (next-pos pos v)
+                         (list (+ v (steps v)) 0)
+                         (steps v)))
+            ((W E) (list (next-pos pos h)
+                         (list 0 (+ h (steps h)))
+                         (steps h))))
+        (when (utils:inside-p grid new-pos)
+          (record-if-new-low-cost
+            visited
+            (make-reached-point
               :POS new-pos
               :MOVES new-moves
-              :COST (+ (value grid new-pos) cost-so-far)
-              :COME-FROM pos)))
+              :COST (next-cost pos move-steps cost-so-far)
+              :COME-FROM (reverse-direction d))))))))
 
-(defun path-find (grid start goal)
-  (let ((cost-so-far (make-hash-table :TEST #'equal)))
-    (loop
-      for queue = (list (make-reached-point :POS start)) then (rest queue)
-      for current = (first queue)
-      while current
-      for pos = (reached-point-pos current)
-      for cost = (reached-point-cost current)
-      when (equal pos goal)
-      return cost
-      do (loop
-           for next in (neighbors grid current)
-           for new-pos = (reached-point-pos next)
-           for new-cost = (reached-point-cost next)
-           for new-moves = (reached-point-moves next)
-           for key = (list new-pos pos new-moves)
-           when (< new-cost (gethash key cost-so-far most-positive-fixnum)) do 
-           (setf (gethash key cost-so-far) new-cost)
-           (nconc queue (list next))
-           finally
-           (sort (rest queue) #'< :KEY #'reached-point-cost)
-           ))
+(defun successors (grid point min-moves max-moves visited)
+  (loop
+    with (v h) = (reached-point-moves point)
+    with come-from = (reached-point-come-from point)
+    for d in (cond ((= v max-moves) '(W E))
+                   ((= h max-moves) '(N S))
+                   (t (remove come-from '(N S W E))))
+    for next-point = (move grid visited point d min-moves)
+    when next-point
+    collect next-point
     ))
+
+(defun gen-successors (grid min-moves max-moves)
+  (let ((visited (make-hash-table :TEST #'equal)))
+    #'(lambda (point)
+        (successors grid point min-moves max-moves visited))))
+
+(defun crucible-successors (grid)
+  (gen-successors grid 1 3))
+
+(defun ultral-crucible-successors (grid)
+  (gen-successors grid 4 10))
+
+(defun sorter (cost-fn)
+  "Return a combiner function that sorts according to cost-fn"
+  #'(lambda (new old)
+      (sort (append new old) #'< :KEY cost-fn)))
 
 (defun make-grid ()
   (utils:make-grid
@@ -82,11 +135,20 @@
 
 (defun part1 ()
   (let ((grid (make-grid)))
-    (destructuring-bind (n m) (array-dimensions grid)
-      (path-find
-        grid
-        '(0 . 0)
-        (cons (1- n) (1- m)))
-      )))
+    (tree-search
+      (list (make-reached-point :POS '(0 . 0)))
+      (reached-p grid)
+      (crucible-successors grid)
+      (sorter #'reached-point-cost))))
 
-(assert (equal (part1) 791)) ; too high
+(defun part2 ()
+  (let ((grid (make-grid)))
+    (tree-search
+      (list (make-reached-point :POS '(0 . 0)))
+      (reached-p grid)
+      (ultral-crucible-successors grid)
+      (sorter #'reached-point-cost))))
+
+;; (assert (equal (reached-point-cost (part1)) 791))
+
+;; (time (part2)) ; 153 seconds
